b74095f745ada6beaceab97f7e401a1e16da8653
Aslıhan Hasar <99024872+miyendisa@users.noreply.github.com>
2022-12-01 16:37:16 +0300

    IndexCalculator.java codes has edited

--- src/IndexCalculator.java
+++ src/IndexCalculator.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 public class IndexCalculator {
     public static void main(String []args){
-        double weight,height,index;
+        double weight,height,index; 
         Scanner scanner=new Scanner(System.in);
+        
         System.out.print("Enter your weight(kg): ");
         weight=scanner.nextDouble();
+        
         System.out.print("Enter your height(m): ");
         height=scanner.nextDouble();
+        
         index=weight/(height*height);
         System.out.print("Your Body-Mass Index:"+ index );
     }
